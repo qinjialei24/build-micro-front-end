c39e7efb615fbcc900884aec85825d08e1149fc7
qinjialei <qinjialei@foxmail.com>
2020-08-06 09:08:36 +0800

    Update

--- qiankun-base/src/main.js
+++ qiankun-base/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI, { Container } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import { registerMicroApps, start } from "qiankun";
+import { registerMicroApps, start, prefetchApps } from "qiankun";
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
@@ -12,17 +12,22 @@ const apps = [
   {
     name: 'vueApp',
     entry: 'http://localhost:10000',//默认会加载这个html，解析里面的 js 动态的执行（子应用必须支持跨域） 使用的是 fetch拉取资源
-    container: '/vue'
+    container: '#vue',
+    activeRule: '/vue'
   },
   {
     name: 'reactApp',
     entry: 'http://localhost:20000',//默认会加载这个html，解析里面的 js 动态的执行（子应用必须支持跨域） 使用的是 fetch拉取资源
-    container: '/react'
+    container: '#react',
+    activeRule: '/react'
   },
 ]
 
 registerMicroApps(apps)
-start()
+start({
+  prefetch: false
+}
+)
 
 
 new Vue({
