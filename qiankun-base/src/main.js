c341c0d771de07957c4e72d99e5dc3f1a505b7f6
qinjialei <qinjialei@foxmail.com>
2020-08-06 08:28:15 +0800

    Update

--- qiankun-base/src/main.js
+++ qiankun-base/src/main.js
@@ -1,13 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.config.productionTip = false;
-
 Vue.use(ElementUI);
 
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  router,
+  render: function (h) { return h(App) }
+}).$mount('#app')
