87f0f402aa182f2fad7b57a59bd5584ac4b6987a
qinjialei <qinjialei@foxmail.com>
2020-08-06 08:16:49 +0800

    Update

--- qiankun-base/src/App.vue
+++ qiankun-base/src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div
+       id="app">
+
+    <el-menu
+             :router="true"
+             mode="horizontal">
+      <router-link>
+      </router-link>
+
+    </el-menu>
+
   </div>
 </template>
 
