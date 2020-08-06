abb462dabd454fd54b14baf93268f7026817c758
qinjialei <qinjialei@foxmail.com>
2020-08-06 11:10:04 +0800

    子应用和基座切换显示

--- qiankun-base/src/App.vue
+++ qiankun-base/src/App.vue
@@ -36,11 +36,17 @@
         </router-view>
 
         <div
-          id="vue">
-        </div>
+          class="child-app"
+          v-show='renderChildApp'>
 
-        <div
-          id="react">
+          <div
+            id="vue">
+          </div>
+
+          <div
+            id="react">
+          </div>
+
         </div>
 
       </div>
@@ -49,6 +55,33 @@
 
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      renderChildApp: false,
+      xxx: true,
+    };
+  },
+  methods: {
+    isRenderChildApp(url) {
+      this.renderChildApp = url.startsWith("/vue") || url.startsWith("/react");
+      console.log("this.renderChildApp: ", this.renderChildApp);
+    },
+  },
+  computed: {},
+  watch: {
+    $route: {
+      handler: function (val, oldVal) {
+        this.isRenderChildApp(val.path);
+      },
+      // 深度观察监听
+      deep: true,
+    },
+  },
+};
+</script>
 
 <style>
 .qiankun-base {
