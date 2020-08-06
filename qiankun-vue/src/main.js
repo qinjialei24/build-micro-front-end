7536aed2bb72c941b04cb79c924a26c8a26c30c2
qinjialei <qinjialei@foxmail.com>
2020-08-06 09:02:40 +0800

    Update

--- qiankun-vue/src/main.js
+++ qiankun-vue/src/main.js
@@ -8,3 +8,30 @@ new Vue({
   router,
   render: function (h) { return h(App) }
 }).$mount('#app')
+
+let instance = null
+function render(props) {
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
+}
+
+if (window.__POWERED_BY_QIANKUN__) {
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+} else {
+  render();
+}
+
+
+export async function bootstrap(props) {
+
+}
+
+export async function mount(props) {
+  render(props)
+}
+
+export async function unmount(props) {
+  instance.$destroy()
+}
